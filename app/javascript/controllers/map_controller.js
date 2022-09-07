ba0883f6eb20dc3ddc7e713ceb8b1064bd19b556
Laura Beckervordersandforth <laura.beckerv@gmail.com>
2022-09-07 15:42:07 +0200

    just for testing...

--- app/javascript/controllers/map_controller.js
+++ app/javascript/controllers/map_controller.js
@@ -8,6 +8,7 @@ export default class extends Controller {
   }
   connect() {
     mapboxgl.accessToken = this.apiKeyValue;
+
     this.map = new mapboxgl.Map({
       container: this.element,
       style: 'mapbox://styles/mapbox/streets-v11', // style URL
@@ -15,13 +16,20 @@ export default class extends Controller {
       // zoom: 9, // starting zoom
     });
     this.#addMarkersToMap()
+    this.#fitMapToMarkers()
   }
 
   #addMarkersToMap() {
     this.markersValue.forEach((marker) => {
       new mapboxgl.Marker()
-      .setLngLat([marker.lng, marker.lat])
+      .setLngLat([ marker.lng, marker.lat ])
       .addTo(this.map);
     })
+  }
+
+  #fitMapToMarkers() {
+    const bounds = new mapboxgl.LngLatBounds()
+    this.markersValue.forEach(marker => bounds.extend([ marker.lng, marker.lat ]))
+    this.map.fitBounds(bounds, { padding: 70, maxZoom: 15, duration: 0 })
   }
 }

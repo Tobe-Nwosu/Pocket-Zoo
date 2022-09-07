d60d48fb7fd5f1d30ea6c1bb5e6744ab2901f700
Nicola Field <nicola.field2@gmail.com>
2022-09-07 14:32:54 +0100

    Adding notes

--- app/controllers/animals_controller.rb
+++ app/controllers/animals_controller.rb
@@ -1,4 +1,7 @@
 class AnimalsController < ApplicationController
+  # TODO: when we have good API, add columns to animal model, change the form name and value
+  # in match.html.erb, and update the index here.
+
   def index
     if params.has_key?(:animal)
       @animals = Animal.where(classification: params[:animal][:classification],

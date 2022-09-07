75d041972f5bd2be9cee6fb31bc0001023135b9f
Laura Beckervordersandforth <laura.beckerv@gmail.com>
2022-09-07 15:06:16 +0200

    marker added

--- app/javascript/controllers/map_controller.js
+++ app/javascript/controllers/map_controller.js
@@ -5,12 +5,18 @@ export default class extends Controller {
   static values = { apiKey: String }
   connect() {
     mapboxgl.accessToken = this.apiKeyValue;
-    const map = new mapboxgl.Map({
+    this.map = new mapboxgl.Map({
       container: this.element,
       style: 'mapbox://styles/mapbox/streets-v11', // style URL
       // center: [-74.5, 40], // starting position [lng, lat]
       // zoom: 9, // starting zoom
     });
+    this.#addMarkersToMap()
+  }
 
+  #addMarkersToMap() {
+    new mapboxgl.Marker()
+      .setLngLat([-65.017, -16.457])
+      .addTo(this.map);
   }
 }

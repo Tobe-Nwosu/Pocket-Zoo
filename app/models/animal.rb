59caff4afe140d7f8c2d9dd7f22b8ab422102d76
Laura Beckervordersandforth <laura.beckerv@gmail.com>
2022-09-07 12:01:35 +0200

    migration: added column address to findings

--- app/models/animal.rb
+++ app/models/animal.rb
@@ -3,4 +3,7 @@ class Animal < ApplicationRecord
 
   validates :name, uniqueness: true
   validates :photo, presence: true
+
+  geocoded_by :address
+  after_validation :geocode, if: :will_save_change_to_address?
 end

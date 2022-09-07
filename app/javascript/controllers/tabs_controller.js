715e6b3184824bff901c69fa90884e8d5f0fcf26
Nicola Field <nicola.field2@gmail.com>
2022-09-07 10:55:30 +0100

    Javascript for tabs

--- app/javascript/controllers/tabs_controller.js
+++ app/javascript/controllers/tabs_controller.js
@@ -2,23 +2,23 @@ import { Controller } from "@hotwired/stimulus"
 
 // Connects to data-controller="tabs"
 export default class extends Controller {
-  static targets = ["findings", "map"]
+  static targets = ["tabcontent", "tablink", "findings", "map", "achievements"]
   connect() {
   }
 
   changeTab(event) {
+    // event.target.classList.toggle("active")
+    this.tabcontentTargets.forEach(tabcontent =>
+      tabcontent.classList.add("d-none"))
+    this.tablinkTargets.forEach(tablink =>
+      tablink.classList.remove('active'))
+    event.target.classList.add('active')
     if (event.target.innerHTML === 'My finds') {
-      event.target.classList.toggle("active")
-
-      console.log(event.target.innerHTML)
-      // this.findingsTarget.classList.remove("d-none")
+      this.findingsTarget.classList.remove('d-none')
     } else if (event.target.innerHTML === 'Map') {
-      event.target.classList.toggle("active")
-      console.log(event.target.innerHTML)
-      // this.mapTarget.classList.remove("d-none")
-    } else if (event.target.innerHTML === 'Achievements') {
-      event.target.classList.toggle("active")
-      console.log(event.target.innerHTML)
+      this.mapTarget.classList.remove('d-none')
+    } else {
+      this.achievementsTarget.classList.remove('d-none')
     }
   }
 }

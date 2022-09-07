9576d091dbe9e798093a9c51d637b5d220075b56
Nicola Field <nicola.field2@gmail.com>
2022-09-07 17:31:03 +0100

    Halfway through implementing pundit

--- app/controllers/findings_controller.rb
+++ app/controllers/findings_controller.rb
@@ -1,17 +1,39 @@
 class FindingsController < ApplicationController
 
   def index
-    @my_findings = Finding.where(user: current_user)
+    @my_findings = policy_scope(Finding)
   end
 
   def show
     @finding = Finding.find(params[:id])
+    authorize @finding
   end
 
   def new
     # @animal = Animal.find(params[:id])
     @finding = Finding.new
+    authorize @finding
   end
+
+  def create
+    @finding = Finding.new(finding_params)
+    @finding.user = current_user
+    authorize @finding
+  end
+
+  def edit
+    authorize @finding
+  end
+
+  def update
+    authorize @finding
+  end
+
+  def destroy
+    authorize @finding
+  end
+
+  private
 
   def finding_params
     params.require(:finding).permit(:date, :photo, :comment)

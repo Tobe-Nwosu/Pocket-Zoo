344bc3371b246e946f27fd143426842520465a09
Nicola Field <nicola.field2@gmail.com>
2022-09-13 14:16:09 +0100

    functioning filter for findings

--- app/models/user.rb
+++ app/models/user.rb
@@ -5,6 +5,7 @@ class User < ApplicationRecord
          :recoverable, :rememberable, :validatable
   has_many :friendships_as_asker, class_name: "Friendship", foreign_key: :asker_id
   has_many :friendships_as_receiver, class_name: "Friendship", foreign_key: :receiver_id
+  has_many :findings
 
   # has_many :achievement ... ?
   validates :username, uniqueness: true

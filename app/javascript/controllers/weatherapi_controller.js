9ecef3a4c0d1d8d020c9080d667edd4f88fbe516
charly philippe <charly.ph@hotmail.fr>
2022-09-22 11:38:18 +0200

    users can sign up now

--- app/javascript/controllers/weatherapi_controller.js
+++ app/javascript/controllers/weatherapi_controller.js
@@ -2,21 +2,37 @@ import { Controller } from "@hotwired/stimulus";
 
 // Connects to data-controller="weatherapi"
 export default class extends Controller {
+  static targets = ["meteo"];
   connect() {
     // console.log("hello from weather controller");
+    let city = "lyon";
+    this.url = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=a160dd207198aea05a45393825008bdf&units=metric`;
+    this.fetchMeteo;
   }
 
-  fetchmeteo() {
-    fetch(
-      // `https://api.openweathermap.org/data/2.5/forecast?lat={45.7}&lon={4.8}&appid=a160dd207198aea05a45393825008bdf`
-      "https://api.openweathermap.org/data/2.5/weather?q=london&appid=a160dd207198aea05a45393825008bdf&units=metric"
-    )
+  fetchMeteo() {
+    fetch(this.url)
       .then((response) => response.json())
-      .then((data) => {
-        console.log(data);
-        // console.log(data.name);
-        // console.log(data.rain);
-        console.log(data.first);
-      });
+      .then(this.displayMeteo.bind(this));
+  }
+
+  displayMeteo(meteo) {
+    console.log(meteo);
+    this.meteoTarget.innerHTML = "";
+    const html = `
+    <img src="./assets/images/green-city.png"> <p> ${meteo.name} <p>
+    <img src="./assets/images/windock.png"> <p> ${meteo.wind.speed} km/h<p>
+    <img src="assets/images/hot.png"> <p> ${meteo.main.temp}°C <p>
+    `;
+    this.meteoTarget.insertAdjacentHTML("beforeend", html);
   }
 }
+
+{
+  /* <p> ${meteo.weather[0].description}<p> */
+}
+// console.log(data.name);
+// console.log(data.wind.speed);
+// console.log(data.wind.speed);
+// console.log(data.weather[0].main);
+// console.log(data.weather[0].description);
